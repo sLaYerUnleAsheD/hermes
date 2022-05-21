fd5153c9b4bf754be2b3fa3b7fb4d5a19a8ec0d9
Mihir Panchal <mihirpanchal2603@gmail.com>
2022-05-21 19:17:49 +0530

    keyboard push problem fixed

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import "./style.css"
 import App from "./App.js"
 import 'font-awesome/css/font-awesome.min.css';
 
+if (document.querySelector(".navbar") == document.activeElement) {
+    document.querySelector(".navbar").class += "absolute"
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />)
